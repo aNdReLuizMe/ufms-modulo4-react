a0e11655d816bbf918279e8dfbbb0b9d67df73f0
aNdReLuizMe <andreluiz.me@gmail.com>
2023-09-12 22:43:30 -0400

    table add

---
+++ app/components/RadioButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const RadioButton = () => {
+   return (
+      <>
+         <div className="form-control">
+            <label className="label cursor-pointer">
+               <span className="label-text">Red pill</span>
+               <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked />
+            </label>
+         </div>
+         <div className="form-control">
+            <label className="label cursor-pointer">
+               <span className="label-text">Blue pill</span>
+               <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked />
+            </label>
+         </div>
+      </>
+   )
+}
+
+export default RadioButton
